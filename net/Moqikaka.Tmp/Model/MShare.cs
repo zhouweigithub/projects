4863dd300b8f5ecb6fc861680174efe95f958f6e
zhouweigithub <137817950@qq.com>
2018-12-31 13:35:19 +0800

    xx

---
+++ net/Moqikaka.Tmp/Model/MShare.cs
@@ -0,0 +1,22 @@
+﻿using Moqikaka.Tmp.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moqikaka.Tmp.Model
+{
+
+    /// <summary>
+    /// 分享记录
+    /// </summary>
+    public class MShare
+    {
+        [TableField]
+        public string DeviceToken { get; set; }
+        [TableField]
+        public int Articalid { get; set; }
+        public DateTime CrTime { get; set; }
+    }
+}
