14a88fe6bafc0fd04fe9947c93ee995dd8f9429c
zhouweigithub <137817950@qq.com>
2019-01-04 20:13:49 +0800

    3

---
+++ net/Moqikaka.Tmp/Model/MDeviceOpenid.cs
@@ -0,0 +1,17 @@
+﻿using Moqikaka.Tmp.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moqikaka.Tmp.Model
+{
+    public class MDeviceOpenid
+    {
+        [TableField]
+        public string DeviceToken { get; set; }
+        [TableField]
+        public string Openid { get; set; }
+    }
+}
---
+++ net/Moqikaka.Tmp/Model/wx/WXLoginResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moqikaka.Tmp.Model.wx
+{
+
+    /// <summary>
+    /// 微信登录返回数据
+    /// </summary>
+    public class WXLoginResult
+    {
+        public string session_key { get; set; }
+        public string openid { get; set; }
+    }
+}
