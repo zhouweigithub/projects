47b425d17166ddfd1ef27a2e3420e2b83fd03bb8
zhouweigithub <137817950@qq.com>
2019-01-15 15:52:32 +0800

    kk

---
+++ net/sunny/Model/Category.cs
@@ -0,0 +1,38 @@
+﻿using Sunny.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunny.Model
+{
+    public class Category
+    {
+        public int id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [TableField]
+        public string name { get; set; }
+        /// <summary>
+        /// 父级id
+        /// </summary>
+        [TableField]
+        public int parent { get; set; }
+        /// <summary>
+        /// 类型0课程 1通用商品
+        /// </summary>
+        [TableField]
+        public short type { get; set; }
+        /// <summary>
+        /// 状态0正常 1禁用
+        /// </summary>
+        [TableField]
+        public short state { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime crtime { get; set; }
+    }
+}
