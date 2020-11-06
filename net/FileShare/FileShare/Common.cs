4c8d854a7c5b13d98ea14fcb4d053b3e14077655
zhouwei <zhouwei@xinqiwan.com>
2020-11-06 15:56:45 +0800

    init file share

---
+++ net/FileShare/FileShare/Common.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace FileShare
+{
+    public class Common
+    {
+        /// <summary>
+        /// 上传目录
+        /// </summary>
+        public const String UploadFolder = "UploadAndShare";
+
+        /// <summary>
+        /// 上传的日志目录名
+        /// </summary>
+        public const String UploadLogFolder = "UploadLog";
+
+        /// <summary>
+        /// 编码格式
+        /// </summary>
+        public static readonly Encoding encoding = Encoding.UTF8;
+    }
+}
